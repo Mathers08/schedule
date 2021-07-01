3ed1251ab57b337b361996d8fb19e2179bb66eed
mathers08 <black.mathers08@mail.ru>
2021-07-01 14:56:09 +0300

    add state

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import Modal from "../Modal/Modal";
 import useModal from '../Modal/useModal';
+import './Day.css';
 
 const Day = () => {
+
     const {isShowing, toggle} = useModal();
+    const [info, setInfo] = useState([]);
+    const [subject, setSubject] = useState('---------');
+    const [office, setOffice] = useState('---------');
+    const [teacher, setTeacher] = useState('---------');
+    const subjectChange = e => setSubject(e.target.value);
+    const officeChange = e => setOffice(e.target.value);
+    const teacherChange = e => setTeacher(e.target.value);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const newItem = {
+            subject,
+            office,
+            teacher
+        }
+        setInfo([...info, newItem]);
+        setSubject('');
+        setOffice('');
+        setTeacher('');
+    }
+
     return (
         <>
-            <td onClick={toggle}>------<br/>------<br/>------</td>
-            <Modal
-                isShowing={isShowing}
-                hide={toggle}
-            />
+            <td onClick={toggle}>{subject}<br/>{office}<br/>{teacher}</td>
+            { isShowing 
+                ? <Modal 
+                    isShowing={isShowing} 
+                    hide={toggle} 
+                    subjectChange={subjectChange}
+                    officeChange={officeChange}
+                    teacherChange={teacherChange}
+                    handleSubmit={handleSubmit}
+                  /> 
+                : null 
+            }
         </>
     );
 }
