04467c64284fbdcfe4849f19e64872c660249241
mathers08 <black.mathers08@mail.ru>
2021-07-01 14:57:43 +0300

    add portal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,28 +1,43 @@
 import React from 'react';
-import ReactDOM, {createPortal} from 'react-dom';
+import {createPortal} from 'react-dom';
 import './Modal.css';
 
-const Modal = ({ isShowing, hide }) => isShowing ? createPortal(
-  <React.Fragment>
-    <div className="modal-overlay"/>
-    <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
-      <div className="modal">
-        <div className="modal-header">
-          <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <form>
-            <p>Предмет</p>
-            <input/>
-            <p>Кабинет</p>
-            <input/>
-            <p>Преподаватель</p>
-            <input/>
-        </form>
-      </div>
-    </div>
-  </React.Fragment>, document.body
-) : null;
+const Modal = (props) => {
+    return createPortal(
+        <React.Fragment>
+            <div className="modal-overlay"/>
+            <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+            <div className="modal">
+                <div className="modal-header">
+                <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={props.hide}>
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <form onSubmit={props.handleSubmit}>
+                    <p>Предмет</p>
+                    <input
+                        autoFocus
+                        type="text"
+                        value={props.subject}
+                        onChange={props.subjectChange}
+                    />
+                    <p>Кабинет</p>
+                    <input
+                        type="text"
+                        value={props.office}
+                        onChange={props.officeChange}
+                    />
+                    <p>Преподаватель</p>
+                    <input
+                        type="text"
+                        value={props.teacher}
+                        onChange={props.teacherChange}
+                    />
+                    <button type="submit">Добавить</button>
+                </form>
+            </div>
+            </div>
+        </React.Fragment>, document.body)
+}
 
 export default Modal;
