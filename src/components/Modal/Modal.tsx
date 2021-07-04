6a3a18b58144d579d93b38c708a980094a4ba3f5
mathers08 <black.mathers.08@mail.ru>
2021-07-04 17:15:54 +0300

    add color-circles

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -18,7 +18,7 @@ const Modal = ({subject, office, teacher, hide, subjectChange, officeChange, tea
   return createPortal(
     <React.Fragment>
       <div className="modal-overlay"/>
-      <div className="modal-wrapper" aria-modal aria-hidden role="dialog">
+      <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
         <div className="modal">
           <div className="modal-header">
             <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
